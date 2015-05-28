ab1dfa9b29b6d846723cfdeb706469ed7dc6ddd7
khairex <khairex@hotmail.com>
2015-05-27 19:16:08 -0700

    pt2

--- src/main/java/com/khairex/woolmod/woolmod.java
+++ src/main/java/com/khairex/woolmod/woolmod.java
@@ -11,8 +11,6 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-import javax.naming.directory.ModificationItem;
-
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class woolmod
 {
